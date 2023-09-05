61bafdfa4130339f685ef37fb616e8b41f4c3f6f
Sergey Mishin <smishin91@yandex.ru>
2023-09-05 23:44:26 +0300

    Add project files.

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using DemoLibrary.Models;
+
+namespace DemoLibrary.DataAccess;
+
+public class DemoDataAccess : IDataAccess
+{
+    private List<PersonModel> people = new();
+
+    public DemoDataAccess()
+    {
+        people.Add(new PersonModel() { Id = 1, FirstName = "Vasya", LastName = "Ivanov" });
+        people.Add(new PersonModel() { Id = 2, FirstName = "Sasha", LastName = "Petrov" });
+    }
+
+    public List<PersonModel> GetPeople()
+    {
+        return people;
+    }
+
+    public PersonModel InsertPerson(string firstName, string lastName)
+    {
+        var p = new PersonModel()
+        {
+            Id = people.Max(p => p.Id) + 1,
+            FirstName = firstName,
+            LastName = lastName
+        };
+
+        people.Add(p);
+
+        return p;
+    }
+}
